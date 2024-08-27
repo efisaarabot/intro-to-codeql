404f5ace006350b9a580740c03a19aaf3d7dd072
efisaarabot <90748589+efisaarabot@users.noreply.github.com>
2024-08-27 10:31:10 +0300

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -10,17 +10,15 @@ def index():
     name = request.args.get('name')
     author = request.args.get('author')
     read = bool(request.args.get('read'))
-
+    
     if name:
-        cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
-        )
+        query = "SELECT * FROM books WHERE name LIKE %s", name
+        cursor.execute(query)
         books = [Book(*row) for row in cursor]
 
     elif author:
-        cursor.execute(
-            "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
-        )
+        query = "SELECT * FROM books WHERE author LIKE %s", author
+        cursor.execute(query)
         books = [Book(*row) for row in cursor]
 
     else:
